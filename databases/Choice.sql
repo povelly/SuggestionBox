24790b9d620ed05541fff71ede1eacea75321335
Sacha Memmi <5996895-3302297@users.noreply.gitlab.com>
2020-10-22 19:06:35 +0200

    Ajout script sql pour générer table connexion

--- databases/Choice.sql
+++ databases/Choice.sql
@@ -4,8 +4,9 @@ create table Choice
 		primary key,
 	voters_cont int default 0 not null,
 	content_choice varchar(200) not null,
-	id_strawpoll int not null,
-	constraint Choice_strawpoll_id_strawpoll_fk
-		foreign key (id_strawpoll) references Strawpoll (id_strawpoll)
+	id_strawpoll int not null
 );
+
+create index Choice_strawpoll_id_strawpoll_fk
+	on Choice (id_strawpoll);
 
--- databases/vote.sql
+++ databases/vote.sql
@@ -1,11 +1,13 @@
 create table vote
 (
 	id_user_transition varchar(50) not null,
-	id_choice_transition int not null,
-	constraint vote_Choice_id_choice_fk
-		foreign key (id_choice_transition) references Choice (id_choice),
-	constraint vote_User_id_user_fk
-		foreign key (id_user_transition) references User (id_user)
+	id_choice_transition int not null
 )
 comment 'Join table between user and choices';
+
+create index vote_Choice_id_choice_fk
+	on vote (id_choice_transition);
+
+create index vote_User_id_user_fk
+	on vote (id_user_transition);
 
