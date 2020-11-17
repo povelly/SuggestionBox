57d2a20c3a47dbbd5e5852779ac7551f8d3b0030
Quentin <quentin.piotrowski@laposte.net>
2020-11-17 15:46:12 +0100

    prmierCPS

--- client/SB/src/app/app.module.ts
+++ client/SB/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
