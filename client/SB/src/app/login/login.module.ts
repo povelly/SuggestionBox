07c11a4d346074a1f6780e640597310c48abf8f1
Quentin <quentin.piotrowski@laposte.net>
2020-11-18 17:05:57 +0100

    delet

--- client/SB/src/app/login/login.module.ts
+++ client/SB/src/app/login/login.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
+//import { RegisterComponent } from './register/register.component';
 import { ResetComponent } from './reset/reset.component';
 
 const routes: Routes = [
   { path: '', component:LoginFormComponent },
-  { path: 'register', component:RegisterComponent },
+  //{ path: 'register', component:RegisterComponent },
   { path: 'reset', component:ResetComponent },
 ];
 
 @NgModule({
-  declarations: [LoginFormComponent, RegisterComponent, ResetComponent],   //Indique les composants qui votn être rajoutés dans ce module
+  declarations: [LoginFormComponent, /*RegisterComponent,*/ ResetComponent],   //Indique les composants qui votn être rajoutés dans ce module
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
