4defc05dcdfc752414e879a154009572a23a7ff9
Quentin <quentin.piotrowski@laposte.net>
2020-11-19 16:48:21 +0100

    formulaire

--- client/SB/src/app/login/login-form/login-form.component.ts
+++ client/SB/src/app/login/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
+  mot = "Bonjour";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
   }
 
 }
