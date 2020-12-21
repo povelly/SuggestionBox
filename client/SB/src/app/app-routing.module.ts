50ee8a668f1f472fcb31ca1d09e0d7de5096d922
Quentin <quentin.piotrowski@laposte.net>
2020-12-21 15:42:36 +0100

    hierarchie ok

--- client/SB/src/app/app-routing.module.ts
+++ client/SB/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 
 const routes: Routes = [
-    { path: 'home', 
-    loadChildren: () => import("./home/home.module").then(m => m.HomeModule)},
+    /*{ path: 'home', 
+    loadChildren: () => import("./home/home.module").then(m => m.HomeModule)},  //Ici modifier le routage 
     { path: 'login',
-    loadChildren: () => import("./login/login.module").then(m => m.LoginModule) }, //url 
+    loadChildren: () => import("./login/login.module").then(m => m.LoginModule) }, //url */
+    { path: '',
+    loadChildren: () => import("./composants/composants.module").then(m => m.ComposantsModule) },
   ];
   
   // sets up routes constant where you define your routes
--- client/SB/src/app/composants/composants.module.ts
+++ client/SB/src/app/composants/composants.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './login-form/login-form.component';
-import { RouterModule, Routes } from '@angular/router';
 import { ResetComponent } from './reset/reset.component';
+import { PageComponent } from './page/page.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component:LoginFormComponent },
+  { path: 'login', component:LoginFormComponent },
   { path: 'reset', component:ResetComponent },
+  { path: 'home', component:PageComponent },
 ];
 
 @NgModule({
-  declarations: [LoginFormComponent, ResetComponent],   //Indique les composants qui votn être rajoutés dans ce module
+  declarations: [LoginFormComponent, ResetComponent, PageComponent],
   imports: [
     CommonModule,
     FormsModule,
     RouterModule.forChild(routes)
   ]
 })
-export class LoginModule { }
+export class ComposantsModule { }
