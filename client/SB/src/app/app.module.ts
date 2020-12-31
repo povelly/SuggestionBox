e63b745cfccf27a4f6b6922456ddd163001dee06
Quentin <quentin.piotrowski@laposte.net>
2020-12-31 14:45:28 +0100

    interceptor importe

--- client/SB/src/app/app.module.ts
+++ client/SB/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -14,6 +14,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips';
+import {InterceptorService} from 'src/app/shared/services/interceptor.service';
 
 
 @NgModule({
@@ -33,7 +34,9 @@ import { MatChipsModule } from '@angular/material/chips';
     MatCheckboxModule,
     MatChipsModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
