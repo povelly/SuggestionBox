a17f3130203446794119a2a1f142a6873435272e
Quentin <quentin.piotrowski@laposte.net>
2021-01-25 15:50:37 +0100

    test 8

--- client/SB/src/app/shared/services/interceptor.service.ts
+++ client/SB/src/app/shared/services/interceptor.service.ts
@@ -16,7 +16,7 @@ export class InterceptorService implements HttpInterceptor{
     //let currentUser = this.authService.currentUserValue;
 //    if (currentUser && currentUser.token) {
       request = request.clone({
-          withCredentials : true   
+          //withCredentials : true   
       });
       console.log("Intercepted")
     return next.handle(request);
