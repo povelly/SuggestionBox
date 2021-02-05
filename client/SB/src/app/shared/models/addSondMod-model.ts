72dfd461f2e8719945a5b4967d59ff09c622d561
Quentin <quentin.piotrowski@laposte.net>
2021-02-05 10:43:07 +0100

    choicesContent

--- client/SB/src/app/shared/models/addSondMod-model.ts
+++ client/SB/src/app/shared/models/addSondMod-model.ts
@@ -3,17 +3,17 @@ export class addSondMod{
     title: string;
     author: string;
     expirationDate: string;
-    choices:string[] = [];
+    choicesContent:string[] = [];
 
     constructor(title:string, author:string, expirationDate: string, choice1: string, choice2: string, choice3: string, choice4: string, choice5: string){
         this.title = title;
         this.author = author;
         this.expirationDate = expirationDate;
-        this.choices.push(choice1);
-        this.choices.push(choice2);
-        if(choice3!="") this.choices.push(choice3);
-        if(choice4!="") this.choices.push(choice4);
-        if(choice5!="") this.choices.push(choice5);
+        this.choicesContent.push(choice1);
+        this.choicesContent.push(choice2);
+        if(choice3!="") this.choicesContent.push(choice3);
+        if(choice4!="") this.choicesContent.push(choice4);
+        if(choice5!="") this.choicesContent.push(choice5);
     }
 
 }
