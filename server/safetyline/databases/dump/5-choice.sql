0438bc12ebc9acdad334f4595a20d1ef44cbfef2
georgemathieu <33687645+georgemathieu@users.noreply.github.com>
2021-02-05 19:05:45 +0100

    sql

--- server/safetyline/databases/dump/5-choice.sql
+++ server/safetyline/databases/dump/5-choice.sql
@@ -6,6 +6,6 @@ create table if not exists choice
 	strawpoll_id int not null,
 	choice_content varchar(200) not null,
 	constraint choice_strawpoll_strawpoll_id_fk
-		foreign key (strawpoll_id) references strawpoll (strawpoll_id)
+		foreign key (strawpoll_id) references strawpoll (strawpoll_id) ON DELETE CASCADE
 );
 
