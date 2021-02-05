b98bd6328006410142ed9093c1039a057c074fe0
Sacha Memmi <5996895-MemmiS@users.noreply.gitlab.com>
2021-02-05 17:57:24 +0100

    bulk insert

--- server/safetyline/src/main/java/com/sorbonne/safetyline/interceptor/SessionInterceptor.java
+++ server/safetyline/src/main/java/com/sorbonne/safetyline/interceptor/SessionInterceptor.java
@@ -1,12 +1,8 @@
 package com.sorbonne.safetyline.interceptor;
 
 import com.sorbonne.safetyline.exception.SessionExpired;
-import org.springframework.util.StringUtils;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-import java.util.Enumeration;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,7 +24,6 @@ public class SessionInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-    	System.out.println(request.getSession().getId());
         HttpSession session = request.getSession(false);
         if(session==null)
         {
