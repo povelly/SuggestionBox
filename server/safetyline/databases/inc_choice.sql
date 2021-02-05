382021736fd57fc17e82ae5b15c5abd086e1c3bc
Sacha Memmi <5996895-MemmiS@users.noreply.gitlab.com>
2021-02-05 19:20:52 +0100

    modification volume docker-compose

--- server/safetyline/databases/inc_choice.sql
+++
@@ -1,7 +0,0 @@
-create definer = root@localhost trigger inc_choice
-	after insert
-	on vote
-	for each row
-	UPDATE choice SET choice.voters_count = choice.voters_count + 1
-         WHERE choice_id = NEW.choice_id;
-
--- server/safetyline/databases/suggestion.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists suggestion
-(
-	suggestion_id int auto_increment
-		primary key,
-	suggestion_content varchar(500) null comment 'txt containing the suggestion',
-	suggestion_creation_date datetime default CURRENT_TIMESTAMP not null,
-	suggestion_author varchar(50) default 'Anonymous' null
-);
-
