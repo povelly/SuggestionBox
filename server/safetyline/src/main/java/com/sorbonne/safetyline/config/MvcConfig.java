5de1a46069cdcd803f42e95d1fb1479c02e4d4a2
Sacha Memmi <5996895-MemmiS@users.noreply.gitlab.com>
2021-02-06 14:16:32 +0100

    session fonctionne à tester plus en profondeur

--- server/safetyline/src/main/java/com/sorbonne/safetyline/config/MvcConfig.java
+++ server/safetyline/src/main/java/com/sorbonne/safetyline/config/MvcConfig.java
@@ -1,17 +1,28 @@
 package com.sorbonne.safetyline.config;
 
 import com.sorbonne.safetyline.interceptor.SessionInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer
 {
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowCredentials(true);
+		registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+				.allowedMethods("GET", "PUT", "DELETE", "POST")
+				.allowedHeaders("*")
+				.allowCredentials(true);
 	}
 	
 	@Override
@@ -20,5 +31,17 @@ public class MvcConfig implements WebMvcConfigurer
 				.addPathPatterns("/accounts","/account", "/accoundDelete",
 						"/suggestions", "/suggestion","/createStrawpoll", "/account/{userId}",
 						"/strawpolls", "/strawpoll");
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(Collections.singletonList("http://localhost:4200")); // Provide list of origins if you want multiple origins
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+		config.setAllowCredentials(true);
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
